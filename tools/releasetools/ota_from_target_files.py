f0b60fe9b271127573d3bf320eff1211bd849e2d
ameerava <42238138+ameerava@users.noreply.github.com>
2019-03-25 14:26:46 +0530

    Update ota_from_target_files.py

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -826,7 +826,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   build_id = target_info.GetBuildProp("ro.build.id")
   build_date = target_info.GetBuildProp("org.aosep.build_date")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
-  device = target_info.GetBuildProp("org.pixelexperience.device")
+  device = target_info.GetBuildProp("org.aosep.device")
 
   script.Print("----------------------------------------------");
   script.Print("              aosep");
